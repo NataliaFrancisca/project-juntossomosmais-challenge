60bddd1817e089a71d2ff5af5c1968d4db438f2f
Natalia Francisca <nathifrancisca@gmail.com>
2022-10-21 12:08:20 -0300

    style: add media screen

--- src/components/Header/HeaderStyle.js
+++ src/components/Header/HeaderStyle.js
@@ -3,25 +3,31 @@ import styled from "styled-components";
 export const HeaderStyle = styled.header`
     padding: 24px 152px;
 
-    span.pagination{
-        padding-left: 4px;
-    }
-
-    h1{
+    & h1{
         font-size: 32px;
         font-weight: 700;
         margin: 24px 0 40px 0;
     }
 
-    section.container-grid-header{
+    & span.pagination{
+        padding-left: 4px;
+    }
+
+    & section.container-grid-header{
         display: grid;
         grid-template-columns: 30% 70%;
-
         justify-content: space-between;
     }
 
-    #container-members{
-        background-color: blue;
-        padding: 10px;
+    @media screen and (max-width: 980px){
+        padding: 24px 120px;
+    }
+
+    @media screen and (max-width: 780px){
+        padding: 24px 100px;
+    }
+
+    @media screen and (max-width: 680px){
+        padding: 24px 20px;
     }
 `
