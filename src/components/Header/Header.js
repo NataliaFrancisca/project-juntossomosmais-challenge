b4830170111fdeb41abdc86518be738da6cd6d4b
Natalia Francisca <nathifrancisca@gmail.com>
2022-11-08 12:23:22 -0300

    feat: get data using fetch

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,19 @@
+import { useState, useEffect } from "react";
 import Members from "../Members/Members";
 import Filter from "../UI/Filter/Filter";
 import { HeaderStyle } from "./HeaderStyle";
 
 const Header = () => {
+
+    const [members, setMembers] = useState();
+
+    useEffect(() => {
+        fetch("http://localhost:9999/members")
+            .then(res => res.json())
+            .then(data => setMembers([...data]))
+            .catch(error => console.error(error))
+    },[])
+
     return(
         <HeaderStyle>
             <span className="pagination">Home &gt; Usuários &gt; Detalhes</span>
@@ -10,7 +21,10 @@ const Header = () => {
 
             <section className="container-grid-header">
                 <Filter />
-                <Members />
+
+                {members ? <Members dataMembers={members} /> 
+                    : <p>Sorry, something wrong with request!</p>
+                }
             </section>
         </HeaderStyle>
     )
