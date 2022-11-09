4fd3863adebd1eaa3c8cf207f774b20e4d7bfed5
Natalia Francisca <nathifrancisca@gmail.com>
2022-11-09 11:52:55 -0300

    feat: change Link to useNavigate

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,30 +1,28 @@
 import { CardStyle } from "./CardStyle";
 
-import {Link} from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 
 const Card = ({dataCard}) => {
     const {name, location, picture, email} = dataCard;
     const {city, state, postcode} = location;
 
+    const navigate = useNavigate();
+
     const getStreet = () => {
         const streetArr = location.street.trim().split(" ");
-
         const getOnlyTheNumber = streetArr[0];
         const getOnlyTheName = streetArr.slice(1, location.street.length).join(" ");
-
         return `${getOnlyTheName}, ${getOnlyTheNumber}`
     }
 
     return(
-        <CardStyle>
+        <CardStyle onClick={() => navigate(`/members/${email}`)}>
             <img src={picture.large} />
 
             <h1>{name.first} {name.last}</h1>
 
             <span className="street">{getStreet()}</span>
             <span className="address-details">{city} <br /> {state} - CEP: {postcode}</span>
-
-            <Link to={`/members/${email}`}>Detalhes</Link>
         </CardStyle>
     )
 }
