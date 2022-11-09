fed84d0032027381c175faa84d25acb71731b15f
Natalia Francisca <nathifrancisca@gmail.com>
2022-11-09 15:09:29 -0300

    feat: function to get the filter values when user go to another page

--- src/components/UI/Filter/Filter.js
+++ src/components/UI/Filter/Filter.js
@@ -1,25 +1,33 @@
-import { useEffect } from "react";
+import { useRef } from "react";
 import { FilterStyle } from "./FilterStyle"
 
 import {useSelector, useDispatch} from "react-redux";
 
 const Filter = () => {
+    const allCheckBoxInputs = Array.from(document.querySelectorAll(`input.checkbox-input`));
 
     const filterReducer = useSelector(state => state.reducerFilter)
     const dispatch = useDispatch();
+    const formRef = useRef();
 
     const onToggleElementFilter = (value) => {
         const checkAlreadyAdded = filterReducer.some(element => element == value.toLowerCase());
         return checkAlreadyAdded ? dispatch({type: "filter/remove", payload: value.toLowerCase()}) : dispatch({type: "filter/add", payload: value.toLowerCase()});
     }
-    
+
+    allCheckBoxInputs.forEach((input) => {
+        const value = input.value.toLowerCase();
+        return filterReducer.includes(value) ? input.checked = true : false;
+    })
+
     return(
         <FilterStyle>
-            <form>
+            <form ref={formRef}>
                 <legend>Por Estado</legend>
 
                 <label>
-                    <input 
+                    <input
+                        className="checkbox-input" 
                         type="checkbox" 
                         name="state" 
                         value="São Paulo" 
@@ -29,7 +37,8 @@ const Filter = () => {
                 </label>
 
                 <label>
-                    <input 
+                    <input
+                        className="checkbox-input" 
                         type="checkbox"
                         name="state"
                         value="Rio de Janeiro"
@@ -39,7 +48,8 @@ const Filter = () => {
                 </label>
 
                 <label>
-                    <input 
+                    <input
+                        className="checkbox-input" 
                         type="checkbox"
                         name="state"
                         value="Minas Gerais"
@@ -49,7 +59,8 @@ const Filter = () => {
                 </label>
 
                 <label>
-                    <input 
+                    <input
+                        className="checkbox-input" 
                         type="checkbox"
                         name="state"
                         value="Espírito Santo" 
@@ -59,7 +70,8 @@ const Filter = () => {
                 </label>
 
                 <label>
-                    <input 
+                    <input
+                        className="checkbox-input" 
                         type="checkbox"
                         name="state"
                         value="Bahia"
