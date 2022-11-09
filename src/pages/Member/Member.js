cb36cbec09ce1ae3e2099c40708c2b9de5658984
Natalia Francisca <nathifrancisca@gmail.com>
2022-11-09 11:00:45 -0300

    feat: create member page

---
+++ src/pages/Member/Member.js
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom"
+import { MemberStyle } from "./MemberStyle";
+
+const Member = () => {
+    const {id} = useParams();
+    const [memberData, setMemberData] = useState()
+
+    useEffect(() => {
+        fetch(`http://localhost:9999/members/${id}`)
+        .then(res => res.json())
+        .then(data => setMemberData(data))
+        .catch(error => console.error(error))
+    },[])
+
+    const formatDate = (date) => {
+        const formatedDate = new Date(date);
+        const [day, month, year] = [formatedDate.getDate(), formatedDate.getMonth(), formatedDate.getFullYear()];
+        return `${day} de ${getMonthName(month)} de ${year}`
+    }
+
+    const getMonthName = (monthNumber) => {
+        const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+        return months[monthNumber];
+    }
+
+    return(
+        <MemberStyle>
+                <header>
+                    <img src={memberData?.picture.large} />
+                    <div className="details-header">
+                        <h1>{memberData?.name.first} {memberData?.name.last}</h1>
+                        <a href={`malito:${memberData?.email}?subject=Assunto`}>{memberData?.email}</a>
+                    </div>
+                </header>
+
+                <hr />
+
+                <main>
+                    <label>Endereço:</label>
+                    <span className="address">{memberData?.location.city} - {memberData?.location.state} - CEP: {memberData?.location.postcode}</span>
+                
+                    <label>Contato:</label>
+                    <ul>
+                        <li>{memberData?.phone}</li>
+                        <li>{memberData?.cell}</li>
+                    </ul>
+
+                    <label>Informações Pessoais:</label>
+                    <ul>
+                        <li>
+                            <label>Data de Nascimento:</label>
+                            <span className="lowercase-label">{formatDate(memberData?.dob.date)}</span>
+                        </li>
+
+                        <li>
+                            <label>Idade:</label>
+                            <span className="lowercase-label">{memberData?.dob.age}</span>
+                        </li>
+
+                        <li>
+                            <label>Data de registro na empresa:</label>
+                            <span className="lowercase-label">{formatDate(memberData?.registered.date)}</span>
+                        </li>
+
+                        <li>
+                            <label>Tempo de empresa:</label>
+                            <span className="lowercase-label">{memberData?.registered.age}</span>
+                        </li>
+                    </ul>
+                </main>
+
+        </MemberStyle>
+    )
+}
+
+export default Member;
---
+++ src/pages/Member/MemberStyle.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+export const MemberStyle = styled.section`
+    width: 100%;
+    min-height: 100vh;
+    height: auto;
+
+    display: flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+
+    & header, main{
+        display: flex;
+        padding: 10px;
+        background-color: #F5F5F5;
+        width: 60%;
+        margin-bottom: 10px;
+    }
+
+    & header{
+        align-items: center;
+
+        & img{
+            width: 160px;
+            height: 160px;
+            border-radius: 50%;
+            background: linear-gradient(to right, blue, green, orange, red);
+            padding: 2px;
+            object-fit: contain;
+        }
+
+        & div.details-header{
+            display: flex;
+            flex-direction: column;
+            padding: 0 10px;
+
+            & h1{
+                font-size: 32px;
+                text-transform: capitalize;
+            }
+        }
+    }
+
+
+    & main{
+        display: flex;
+        flex-direction: column;
+        padding: 20px;
+
+        & label{
+            font-weight: bold;
+            margin-top: 10px;
+        }
+
+        & span, li{
+            text-transform: capitalize;
+        }
+
+        & li{
+            margin: 4px 0 4px 20px;
+        }
+
+        & li > span{
+            margin-left: 4px;
+        }
+
+        & span.lowercase-label{
+            text-transform: lowercase;
+        }
+    }
+       
+
+
+       
+
+  
+`
