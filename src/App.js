e64566e6981f26a95e6c5207d94868662096c7c5
Natalia Francisca <nathifrancisca@gmail.com>
2022-11-09 10:59:24 -0300

    feat: add route member

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import { GlobalStyle } from "./styles/Global";
 
+import Member from "./pages/Member/Member";
 import Nav from "./components/Nav/Nav";
 import Header from "./components/Header/Header";
+
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
 
-      <Nav />
-      <Header />
+      <BrowserRouter>
+        <Routes>
+       
+          <Route path="/" 
+          element={
+              <>
+                <Nav />
+                <Header />
+              </>} 
+          />
+
+          <Route path="/members/:id" element={<Member />} />
+         
+        </Routes>
+
+      </BrowserRouter>
+    
     </div>
   );
 }
