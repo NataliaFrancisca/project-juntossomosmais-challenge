c2eaa6eb00dfbede862bd09b38281e40af3addc9
Natalia Francisca <nathifrancisca@gmail.com>
2022-11-10 16:09:35 -0300

    feat: import styled component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,13 +2,15 @@ import Nav from "../../components/Nav/Nav";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 
+import { HomeStyle } from "./HomeStyle";
+
 const Home = () => {
     return(
-        <main>
+        <HomeStyle>
             <Nav />
             <Header />
             <Footer />
-        </main>
+        </HomeStyle>
     )
 }
 
