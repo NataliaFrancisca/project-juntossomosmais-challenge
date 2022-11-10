bce8e349621fd4723fa833a2759d10ef1497fb93
Natalia Francisca <nathifrancisca@gmail.com>
2022-11-10 16:09:57 -0300

    style: fix page to fit all page

---
+++ src/pages/Home/HomeStyle.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HomeStyle = styled.main`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    height: auto;
+    justify-content: space-between;
+`
