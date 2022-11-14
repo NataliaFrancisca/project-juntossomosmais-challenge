5e81f3b4a2af5da445ca47c47864a10be6510b28
Natalia Francisca <nathifrancisca@gmail.com>
2022-11-14 10:32:14 -0300

    style: change html tag element

--- src/pages/Home/HomeStyle.js
+++ src/pages/Home/HomeStyle.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const HomeStyle = styled.main`
+export const HomeStyle = styled.div`
     display: flex;
     flex-direction: column;
     min-height: 100vh;
